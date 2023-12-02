46bcab178f878ccaddcf15a5ac1a9e21fa0d44d1
Yuval Dolev <uvero3@gmail.com>
2023-12-02 15:03:37 +0200

    Created routing

---
+++ src/App/pages/Page404.tsx
@@ -0,0 +1,10 @@
+import { Paper } from "@mui/material";
+import React from "react";
+
+export default function Page404() {
+	return (
+		<Paper>
+			{"This is not the page you're looking for"}
+		</Paper>
+	);
+}

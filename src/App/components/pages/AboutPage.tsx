5b56db9ed951437810a6657cb55cd59ce822d032
Yuval Dolev <uvero3@gmail.com>
2023-12-04 16:09:22 +0200

    Added acknowledgements to about page

--- src/App/components/pages/AboutPage.tsx
+++ src/App/components/pages/AboutPage.tsx
@@ -2,18 +2,54 @@ import React from "react";
 import { appName } from "../../../meta";
 import { ExternalLink } from "../utils/ExternalLink";
 
+function npm(name: string, usage?: string) {
+	return {
+		name: name,
+		link: "https://www.npmjs.com/package/" + name,
+		...{ usage }
+	};
+}
+
+const using: { name: string, link: string, usage?: string }[] = [
+	{ name: "MUI (Material UI)", link: "http://mui.com", usage: "for UI" },
+	{ name: "React", link: "https://react.dev/", usage: "to run" },
+	{ name: "Github Pages", link: "https://pages.github.com/", usage: "as a host" },
+	npm("@mui/icons-material", "for icons"),
+	npm("@fontsource/roboto", "for app font"),
+	npm("@fontsource/caveat", "for logo font"),
+	npm("@mui/x-charts", "for charts"),
+	npm("apportionment", "for apportionment calculations"),
+	npm("easy-peasy", "for state management"),
+	npm("mui-color-input", "for color input"),
+	npm("@faker-js/faker", "to give new parties random color and a fun random name"),
+	npm("@emotion/react"),
+	npm("uniqid"),
+	npm("typescript", "type safety")
+];
+
+function Using() {
+	return (
+		<ul>
+			{using.map((x, i) => (<li key={i}>
+				<ExternalLink to={x.link}>{x.name}</ExternalLink>
+				{x.usage && " " + x.usage}
+			</li>))}
+		</ul>
+	);
+}
+
 export default function AboutPage() {
 	return (
 		<div>
 			<h1>Welcome to {appName}!</h1>
-			
+
 			<h2>What is this?</h2>
 			<p>
 				{appName} is an app that can be used to calculate&nbsp;
 				<ExternalLink to="https://en.wikipedia.org/wiki/Apportionment_(politics)">
 
 					parliamentary apportioment</ExternalLink>
-					&nbsp;
+				&nbsp;
 				from elections results.
 			</p>
 
@@ -22,8 +58,8 @@ export default function AboutPage() {
 				<ExternalLink to="https://www.reddit.com/r/imaginaryelections/"  >
 					r/imaginaryelections
 				</ExternalLink>,
-			a subreddit dedidcated to... well, imaginary elections.
-			Elections in alternative timelines, fictional universes, and what-ifs, in the future, past, present.
+				a subreddit dedidcated to... well, imaginary elections.
+				Elections in alternative timelines, fictional universes, and what-ifs, in the future, past, present.
 			</p>
 
 			<h2>Wait that{"'"}s a thing?</h2>
@@ -46,7 +82,7 @@ export default function AboutPage() {
 				({"link will be here - if you're seeing this message, give me a bit of time to update this!"}).
 				Sadly, not every idea will be made. For example, I was asked to support&nbsp;
 			<ExternalLink to="https://en.wikipedia.org/wiki/Single_transferable_vote">
-				Single Transferrable Vote method	
+					Single Transferrable Vote method
 			</ExternalLink>, which I {"don't"} think will likely go well with how this app works.
 			</p>
 
@@ -54,10 +90,10 @@ export default function AboutPage() {
 			<p>No promises, but maybe.</p>
 
 			<h2>The design is kinda meh. Things {"aren't"} aligned properly.</h2>
-			<p>Yes, 
+			<p>Yes,
 				{"I'm"} not much of an expert on&nbsp;
 				<ExternalLink to="https://en.wikipedia.org/wiki/CSS">
-				CSS</ExternalLink>, yet.
+					CSS</ExternalLink>, yet.
 			</p>
 
 			<h2>And it {"doesn't"} work on my phone!</h2>
@@ -67,12 +103,24 @@ export default function AboutPage() {
 
 			<h2>Are you taking&nbsp;
 				<ExternalLink to="https://en.wikipedia.org/wiki/Distributed_version_control#Pull_requests">
-				pull-requests</ExternalLink>?
+					pull-requests</ExternalLink>?
 			</h2>
 			<p>I {"don't"} know - worth a shot!</p>
 
+			<h2>What does this app use?</h2>
+			<p>
+				In no particular order:
+				<Using />
+			</p>
+			<p>
+				{"If you think I'm missing an acknowledgement here, " +
+					"sorry in advance, and please let me know via Github Issues. "}
+				A full auto-generated (by yarn) disclosure can be found in the github repository.
+			</p>
+
 			<h2>Did you have to make the about page in Q&amp;A format?</h2>
 			<p>I... OK, that{"'"}s enough from you.</p>
+
 		</div>
 	);
 }

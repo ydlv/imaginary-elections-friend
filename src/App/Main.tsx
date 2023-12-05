830f4e6c52669eb5ce2dd4ce784ad90c5a2b039b
Yuval Dolev <uvero3@gmail.com>
2023-12-05 16:47:27 +0200

    phone compatability warning

--- src/App/Main.tsx
+++ src/App/Main.tsx
@@ -1,12 +1,29 @@
-import { ThemeProvider } from "@mui/material";
+import { Box, Button, Modal, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import { StoreProvider } from "easy-peasy";
 import { store } from "./store/store";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "./components/routing/AppRouter";
 import { version } from "../meta";
+import { modalContentStyle } from "./components/modals/modal-style";
 
 function Main() {
+	const [width, setWidth] = useState<number>(window.innerWidth);
+
+	function handleWindowSizeChange() {
+		setWidth(window.innerWidth);
+	}
+	useEffect(() => {
+		window.addEventListener("resize", handleWindowSizeChange);
+		return () => {
+			window.removeEventListener("resize", handleWindowSizeChange);
+		};
+	}, []);
+
+	const isMobile = width <= 768;
+	console.log({isMobile});
+	const [showModal, setShowModal] = useState(isMobile);
+
 	return (
 		<>
 			<StoreProvider store={store}>
@@ -17,6 +34,17 @@ function Main() {
 			<footer>
 				Version {version}
 			</footer>
+			<Modal open={showModal}>
+				<Box sx={modalContentStyle}>
+					<p>
+						{"You seem to be on a mobile phone. Mobile phones are not yet supported on this app, but may"}
+						{" be in the future. Sorry about that."}</p>
+					<p>{"You can still view the app, but it probably won't work."}</p>
+					<Button variant="contained" color="error"
+						onClick={() => setShowModal(false)}
+					>View app anyway</Button>
+				</Box>
+			</Modal>
 		</>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ import "@fontsource/roboto/700.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
 	<React.StrictMode>
 		<Main />
